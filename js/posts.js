5051a8811392c64cbb6911ace4e010afad9ced22
Arifu Islam Adil <adil.islam.arif@gmail.com>
2022-01-08 15:06:52 +0600

    api intro

---
+++ js/posts.js
@@ -0,0 +1,23 @@
+function loadPosts() {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+        .then(res => res.json())
+        .then(data => displayPosts(data))
+}
+loadPosts()
+
+function displayPosts(posts) {
+
+    const postContainer = document.getElementById("post-container");
+
+    for (const post of posts) {
+        const div = document.createElement("div");
+        div.classList.add("post")
+        div.innerHTML = `
+        <h1>Id : ${post.id}</h1>
+        <h1>New Title: ${post.title}</h1>
+        <p>New descriptipn: ${post.body}</p>
+        `
+        postContainer.appendChild(div)
+    }
+
+}
