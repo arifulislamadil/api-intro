8b960ae834d838e564208e4904ee3a27d9003388
Arifu Islam Adil <adil.islam.arif@gmail.com>
2022-01-08 18:07:59 +0600

    added few lines

--- js/posts.js
+++ js/posts.js
@@ -6,18 +6,15 @@ function loadPosts() {
 loadPosts()
 
 function displayPosts(posts) {
-
     const postContainer = document.getElementById("post-container");
-
     for (const post of posts) {
         const div = document.createElement("div");
         div.classList.add("post")
         div.innerHTML = `
         <h1>Id : ${post.id}</h1>
         <h1>New Title: ${post.title}</h1>
-        <p>New descriptipn: ${post.body}</p>
-        `
+        <p>New descriptipn: ${post.body}</p> `
         postContainer.appendChild(div)
     }
-
 }
+
